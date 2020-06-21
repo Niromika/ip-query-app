3c2d9b263b423545573c10a61e1a8de6cbab9a08
Nir Adar <niromika88@gmail.com>
2020-06-21 14:05:50 +0300

    Updates

---
+++ precache-manifest.e5e620310697b25863dc047ee2e91191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e06e1e9ed27df42aac4d83d6fb10efb2",
+    "url": "/ip-query-app/index.html"
+  },
+  {
+    "revision": "d63cae997e9444b54222",
+    "url": "/ip-query-app/static/css/main.e7c93453.chunk.css"
+  },
+  {
+    "revision": "2bb71a28517feafb4110",
+    "url": "/ip-query-app/static/js/2.8fe317eb.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/ip-query-app/static/js/2.8fe317eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63cae997e9444b54222",
+    "url": "/ip-query-app/static/js/main.855016f1.chunk.js"
+  },
+  {
+    "revision": "2617a6703f1604ee8939",
+    "url": "/ip-query-app/static/js/runtime-main.cf66d348.js"
+  }
+]);
